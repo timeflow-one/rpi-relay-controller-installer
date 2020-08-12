864e9012d50542e3fe012ec07e787f5919247cfa
Alexander Kondrev <kondrev@trinity.local>
2020-08-12 16:51:40 +0300

    Update init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,11 @@
-# TODO getting builded frontend
-# curl > page/
+# gitting builded fronted
+curl https://github.com/timeflow-one/rpi-relay-controller-console/releases/download/2.0.0/rpi-relay-controller-console.tar > page/rpi-relay-controller-console.tar
+cd page
+tar -xf rpi-relay-controller-console.tar .
+rm rpi-relay-controller-console.tar
+cd ..
 
-# set timeflow controller
+# set timeflow server controll
 sed -i 's/{TIMEFLOW_PREFIX_SERVER}/ru/g' nginx/controller.conf
+
+docker-compose up
