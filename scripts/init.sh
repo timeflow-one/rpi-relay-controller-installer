0d459ee933b48a106d9480d0c5547397c8e24cb5
Alexander Kondrev <kondrev@trinity.local>
2020-08-20 13:51:32 +0300

    Update init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 # getting builded fronted
-curl -L https://github.com/timeflow-one/rpi-relay-controller-console/releases/download/2.0.0/rpi-relay-controller-console.tar > page/rpi-relay-controller-console.tar
+curl -L https://github.com/timeflow-one/rpi-relay-controller-console/releases/download/$(curl -s https://raw.githubusercontent.com/timeflow-one/rpi-relay-controller-console/master/package.json | grep -Po -m 1 '\d.\d.\d')/rpi-relay-controller-console.tar > page/rpi-relay-controller-console.tar
 cd page
 tar -xf rpi-relay-controller-console.tar
 rm rpi-relay-controller-console.tar
@@ -7,8 +7,8 @@ cd ..
 
 # getting docker image
 # TODO replace ... to archive location
-curl ... > rpi-relay-controller.tar
-docker load -i rpi-relay-controller.tar
+curl ... > /tmp/rpi-relay-controller.tar
+docker load -i /tmp/rpi-relay-controller.tar
 
 # set timeflow server controll
 sed -i 's/{TIMEFLOW_PREFIX_SERVER}/ru/g' nginx/controller.conf
